92437b438fb14c79b20ed675668154bc86a833b8
Richard Marchesi <richard.marchesi@gmail.com>
2021-12-31 15:18:15 +0100

    Update doks_create.sh

--- doks_create.sh
+++ doks_create.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "make sure DO_ACCESS_TOKEN, DOKS_NAME and DO_REGION are set"
+# set sensible defaults whcih can be overridden as needed.
+# Cassandra is very memory hungary, still needs tweaking, ideally 8gb RAM is best.
+export DO_REGION=ams3
+export DO_SIZE=s-2vcpu-4gbs
+
+echo "make sure DO_ACCESS_TOKEN and DOKS_NAME are set"
 echo "creating DOKS Cluster with deafult paramters"
 
-doctl auth init --access-token $DO_ACCESS_TOKEN
 doctl kubernetes cluster create $DOKS_NAME --region $DO_REGION --size $DO_SIZE --access-token $DO_ACCESS_TOKEN
