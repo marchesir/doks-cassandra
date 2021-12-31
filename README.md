fcb3090e6750687e1bd89b371f4393872073f364
Richard Marchesi <richard.marchesi@gmail.com>
2021-12-31 01:56:12 +0100

    added k8s native config for cassandra cluster

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ use DOKS defaults
 3 nodes
 s-1vcpu-2gb
 
+kubectl patch storageclass do-block-storage -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+kubectl patch storageclass fast -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
--- doks_create.sh
+++ doks_create.sh
@@ -4,4 +4,4 @@ echo "make sure DO_ACCESS_TOKEN, DOKS_NAME and DO_REGION are set"
 echo "creating DOKS Cluster with deafult paramters"
 
 doctl auth init --access-token $DO_ACCESS_TOKEN
-doctl kubernetes cluster create $DOKS_NAME --region $DO_REGION --access-token $DO_ACCESS_TOKEN
+doctl kubernetes cluster create $DOKS_NAME --region $DO_REGION --size $DO_SIZE --access-token $DO_ACCESS_TOKEN
